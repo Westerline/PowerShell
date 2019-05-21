6f4c0aeb8bc59625a531bd371ccda9b211b13ef7
Wesley Esterline <esterline.wj.s@gmail.com>
2019-05-21 12:27:11 +1200

    Update Test-Ports.ps1

--- Test-Ports.ps1
+++ Test-Ports.ps1
@@ -4,4 +4,25 @@ Foreach ($Computer in $Computers) {
 
 Test-NetConnection $Computer -CommonTCPPort HTTP | Out-File -FilePath 'C:\temp\PSRemote_BOS.txt' -Append
 
+
+
+$PortQry = Set-location '.\Utilities\Sysinternals\PortQryUI'
+$OutputFile = '.\Log\NZP-Test_Octgn\'
+
+                    #DNS#
+.\PortQry.exe -n 0.0.0.0 -p UDP -e 53 -l "$OutputFile\DNS_1.log"
+                    #####
+
+
+
+                    #HTTP#
+.\PortQry.exe -n hostname.com -p TCP -e 80 -l "$OutputFile\HTTP_1.log"
+                    ######
+
+
+
+                    #FTP#
+.\PortQry.exe -n hostname -p TCP -e 20,21 -l "$OutputFile\FTP_1.log"
+                    #####
+                    
 }
