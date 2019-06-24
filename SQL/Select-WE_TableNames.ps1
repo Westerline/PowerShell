a25b17183cbbdfe57f76957918ecf5097dae941e
Wesley Esterline <esterline.wj.s@gmail.com>
2019-06-25 11:21:23 +1200

    Add PowerShell SQL Scripts, Select TableNames and Set User Roles

---
+++ SQL/Select-WE_TableNames.ps1
@@ -0,0 +1,71 @@
+﻿<#
+.SYNOPSIS
+    This is a very short summary of the script.
+
+.DESCRIPTION
+    This is a more detailed description of the script. # The starting ErrorActionPreference will be saved and the current sets it to 'Stop'.
+
+.PARAMETER UseExitCode
+    This is a detailed description of the parameters.
+
+.EXAMPLE
+    Scriptname.ps1
+
+    Description
+    ----------
+    This would be the description for the example.
+
+.NOTES
+    Author: Wesley Esterline
+    Resources: 
+    Updated:     
+    Modified from Template Found on Spiceworks: https://community.spiceworks.com/scripts/show/3647-powershell-script-template?utm_source=copy_paste&utm_campaign=growth
+#>
+
+[CmdletBinding()]
+
+Param (
+
+    [Parameter(Mandatory = $False)]
+    [Alias('Transcript')]
+    [string]$TranscriptFile
+
+)
+
+Begin {
+    Start-Transcript $TranscriptFile  -Append -Force
+    $StartErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'Stop'
+    $User = ""
+    $SQL_Credential = Read-Host -AsSecureString
+
+}
+
+Process {
+
+    Try {
+       
+        Invoke-Sqlcmd -query "SELECT TABLE_NAME FROM <DATABASE_NAME>.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
+
+    }
+
+    Catch [SpecificException] {
+        
+    }
+
+    Catch {
+
+
+    }
+
+    Finally {
+
+    }
+
+}
+
+End {
+
+    $ErrorActionPreference = $StartErrorActionPreference
+    Stop-Transcript | Out-Null
+}
