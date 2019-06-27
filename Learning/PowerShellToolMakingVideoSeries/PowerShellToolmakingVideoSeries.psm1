28797ac3009b6d7e3d2ac93172f375a606b7fba3
Wesley Esterline <esterline.wj.s@gmail.com>
2019-06-27 16:14:37 +1200

    Video Series 2/3 Complete

---
+++ Learning/PowerShellToolMakingVideoSeries/PowerShellToolmakingVideoSeries.psm1
@@ -0,0 +1,104 @@
+<#
+Comment based help goes at the top.
+
+Tools should do one thing. A controller script is what takes a bunch of tools and gives them context and purpose. Controller scripts should be really simple and have minimal logic. Improved debugging and modularization.
+
+Multiple functions can be combined together into a module. Modules have the .psm1 extension.
+
+Saved as a function so that we can have it be part of a PowerShell module. Best practice for auto-loading modules is to modify PSModule path with an additional location for modules. Do this by appending the environment variables.
+
+Modules can have more than one file, this is where manifest files come in. Loads files in this order: .psd1, .dll, .psm1. To create a manifest, use the following:
+    Set-Location -Path <Path to PowerShell Module>
+    New-ModuleManifest -Path ModuleName.psd1 -RootModule ModuleName.psm1
+Manifest files are the prefered place to put in minimum requirements for the module to load.
+Manifest files should contain all of the meta-data for a module.
+Manifest files are also the prefered place to create your own custom formatting instructions for your object output.
+Manifest files should be used to explicitly state which functions from your module should be exported
+#>
+
+
+Function PowerShellToolmakingVideoSeries {
+
+    #Enables common parameters
+    #SupportsShouldProcess enables -whatif and -confirm parameters. You only need this if your commands are changing things.
+    [CmdletBinding()]
+    param (
+
+        #Allows us to accept parameter input from the pipeline by object type and by property name.
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage = "The. Computer. Name.")]
+
+        #You can create aliases that will allow you to bind ValueFromPipelineByPropertyName. For example, if you get AD Objects, and it comes back with 'CN' property containing the names of users, 'CN' alias will bind to that input.
+        [Alias('HostName', 'CN')]
+        #[] used to allow for multi-object input.
+        [String[]]$ComputerName
+
+    )
+
+    #Begin, Process, End supports the pipeline input run scenario. The ForEach supports the parameter input run scenario.
+    Begin { }
+
+    Process {
+        #Foreach used, rather than loading the objects and piping each one to the command, because it supports a broader range of input patterns.
+        ForEach ($Computer in $ComputerName) {
+
+            #If an error were to occur, $Session is likely place. We want to wrap all the way through to Write-Output in a try because we don't want the rest of the code to execute.
+            Try {
+                #Instead of setting up two CIM connections, we're setting up one CIM session for OS and CS to use
+                $Session = New-CimSession -ComputerName $Computer -ErrorAction Stop
+                $OS = Get-CimInstance -CimSession $Session -ClassName Win32_OperatingSystem
+                $CS = Get-CimInstance -CimSession $Session -ClassName Win32_ComputerSystem
+        
+                <#
+            Instead of outputting two different object types, we're going to create a hash table of the properties we want from our two objects.
+            Non-ordered hash tables are more memory efficient
+            #>
+                $Property = @{Computername = $ComputerName
+                    Stauts                 = 'Connected'
+                    SPVersion              = $OS.ServicePackMajorVersion
+                    OSVersion              = $OS.Version
+                    Model                  = $CS.Model
+                }
+        
+            }
+
+            Catch {
+            
+                #We will only get verbose output if CmdletBinding is enabled and the -Verbose parameter is used. -Verbose parameter gets passed to all commands.
+                #If you want to create a section header, the verbose pipeline is a good place for that
+                Write-Verbose '---------------------------------------------'
+                Write-Verbose "Unable to establish CIM instance to $Computer"
+
+                #We will still generate an output object, but this can give us more information about the error.
+                $Property = @{Computername = $ComputerName
+                    Status                 = 'Disconnected'
+                    SPVersion              = 'Null'
+                    OSVersion              = 'Null'
+                    Model                  = 'Null'
+                }
+
+            }
+
+            Finally {
+                <#Next, we create a PSObject that has either the try or catch properties we retrieved above. This is saved to a variable for a couple reasons 
+            1. Lets us change $Object before outputting
+            2. It's good practice to explicitly write your output, that way you can see where in your code to start investigating if there are output errors.
+            
+            Don't use Format commands within your script because this doesn't produce a PSObject. 
+
+            #>
+                $Object = New-Object -TypeName PSObject -Property $Property
+                #It is possible to create a custom format view as part of your module. If you do this, you'll want to convert $Object to a different custom object name. This object name will then correlate to the name in your custom format XML.
+                $Object.PSObject.TypeNames.Insert(0,'My.Awesome.Object')
+                Write-Output $Object
+            }
+
+        }
+    
+    }
+
+    End { }
+
+}
--- SQL/Deploy-WE_SQLScript.ps1
+++ SQL/Deploy-WE_SQLScript.ps1
@@ -1,113 +1,39 @@
-﻿<#
-.SYNOPSIS
-    This is a very short summary of the script.
-
-.DESCRIPTION
-    This is a more detailed description of the script. # The starting ErrorActionPreference will be saved and the current sets it to 'Stop'.
-
-.PARAMETER UseExitCode
-    This is a detailed description of the parameters.
-
-.EXAMPLE
-    Scriptname.ps1
-
-    Description
-    ----------
-    This would be the description for the example.
-
-.NOTES
-    Author: Wesley Esterline
-    Resources: 
-    Updated:     
-    Modified from Template Found on Spiceworks: https://community.spiceworks.com/scripts/show/3647-powershell-script-template?utm_source=copy_paste&utm_campaign=growth
-#>
-
-[CmdletBinding()]
+﻿[CmdletBinding()]
 
 Param (
-
-    [Parameter(Mandatory = $False)]
-    [Alias('Transcript')]
-    [string]$TranscriptFile
-
+    [String[]] $Hostname,
+    [String] $ServerInstance = '',
+    [String] $Database,
+    [String] $SQLScriptPath
+    
 )
 
-Begin {
-    Start-Transcript $TranscriptFile  -Append -Force
-    $StartErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = 'Stop'
-    $Files = 'C:\temp\test.sql'
-    $LogPath1 = 'C:\temp\Deploy-Tables_Check_SQL.log'
+Try {
 
-}
+    $Script = Invoke-Sqlcmd -ServerInstance $ServerInstance -HostName $HostName -Database $Database -InputFile $SQLScriptPath -ErrorAction Stop
 
-Process {
-
-    $Test.GetEnumerator() | ForEach-Object {
-
-        Try {
-       
-            $message = 'Querying {0} @ {1}...' -f $_.key, $_.value
-
-            Write-Output $message
-
-            $IP = $_.value
-
- 
-
-
-            Copy-Item $Files -Destination "\\$IP\c$\temp\test.sql" -Force  -ErrorAction Stop
-        
-            PSEXEC \\$IP -accepteula -nobanner SQLCMD -s .\MSSQL -i "C:\temp\test.sql" | Format-Table -AutoSize
-
-
-
-
-            $Properties = [Ordered] @{
-                Computer     = $_.key
-                IP           = $_.value
-                Availability = 'TRUE'
-                Results      = $Results
-            }
-
-
-        }
-
-        Catch [SpecificException] {
-        
-        }
-
-        Catch {
-
-            $message = 'Failed to connect to {0} @ {1}...' -f $_.key, $_.value
-
-            Write-Output $message
-
-            $IP = $_.value
-
-
-
-            $Properties = [Ordered] @{
-                Computer     = $_.key
-                IP           = $_.value
-                Availability = 'FALSE'
-                Results      = 'NULL'
-            }
-
-        }
-
-        Finally {
-
-            (New-Object -TypeName PSObject -Property $Properties) | Out-File $LogPath1 -Append
-
-        }
-    
+    $Property = @{Hostname = $Hostname
+        Database      = $Database
+        Status        = 'Connected'
+        ScriptResults = $Script
     }
 
 }
 
-End {
+Catch {
 
-    $ErrorActionPreference = $StartErrorActionPreference
-    Stop-Transcript | Out-Null
+    $Property = @{Hostname = $Hostname
+        Database      = $Database
+        Status        = 'Disconnected'
+        ScriptResults = 'Null'
+    }
+
+}
+
+
+Finally {
+
+    $Object = New-Object -TypeName psobject -Property $Property
+    Write-Output $Object
+
 }
