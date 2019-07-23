293e3a305e611ed536d90668bb688d3b67ae485f
Wesley Esterline <esterline.wj.s@gmail.com>
2019-07-23 16:22:03 +1200

    Base Code Rewrite

--- Shell Configuration (In-Progress)/Set-WE_CustomPrompt (In-Progress).ps1
+++
@@ -1,5 +0,0 @@
-function Set-WE_CustomPrompt {
-    $Date = (Get-Date).ToShortDateString()
-    $Time = (Get-Date).ToShortTimeString()
-    "[$env:COMPUTERNAME] {$date $time}:> "
-}
---
+++ Shell Configuration/New-WE_SelfSignedCodeCertificate.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.Description
+    https://blogs.u2u.be/u2u/post/creating-a-self-signed-code-signing-certificate-from-powershell
+    Generates self-signed powershell code signing certificate. Should only be used in test environments.
+    Certificate must be moved to the local machine's trusted root and trusted publisher stores for the code to be executed.
+#>
+
+Param(
+    [Int] $Duration,
+    [String] $Subject = "PowerShell Code Signing Certificate",
+    [String] $CertStoreLocation = "Cert:\LocalMachine\My"
+)
+
+
+Try {
+    $NotAfter = $([datetime]::now.AddYears($Duration))
+    $Certificate = New-SelfSignedCertificate -Subject $Subject -Type CodeSigningCert -NotAfter $NotAfter -CertStoreLocation $CertStoreLocation
+    $TrustedRoot = Copy-Item -Path $Certificate.PSPath -Destination "Cert:\LocalMachine\Root"
+    $TrustedPublisher = Move-Item -Path $Certificate.PSPath -Destination "Cert:\LocalMachine\TrustedPublisher"
+    $Property = @{
+        Thumbprint       = $Certificate.Thumbprint
+        TrustedRoot      = $TrustedRoot
+        TrustedPublisher = $TrustedPublisher
+    }
+}
+
+Catch { 
+    $Property = @{
+        Thumbprint       = 'Null'
+        TrustedRoot      = 'Null'
+        TrustedPublisher = 'Null'
+    }
+}
+
+Finally {
+    $Object = New-Object -TypeName PSObject -Property $Property
+    Write-Output $Object
+}
